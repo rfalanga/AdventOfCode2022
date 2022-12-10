49f3ae7371fb893a00f56bae86305ccc2c3264fd
Rod Falanga <rfalanga@users.noreply.github.com>
2022-12-10 09:02:47 -0700

    added input file and beginning class to process it

---
+++ AdventOfCode2022/AdventOfCode2022/Day1Part1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day1Part1
+    {
+        public List<int> Calories { get; set; }
+
+        public Day1Part1()
+        {
+            Calories= new List<int>();
+        }
+
+        public int TotalCalories()
+        { return Calories.Sum(); }
+    }
+}
