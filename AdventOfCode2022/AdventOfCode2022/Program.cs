c9b9e3b0ffdedf47f7c4052c059744a5239d8914
Rod Falanga <rfalanga@users.noreply.github.com>
2022-12-14 19:20:10 -0700

    I've finished Day 1, Part 2; need to test it

--- AdventOfCode2022/AdventOfCode2022/Program.cs
+++ AdventOfCode2022/AdventOfCode2022/Program.cs
@@ -2,11 +2,12 @@
 
 using AdventOfCode2022;
 
-List<ElfFood> day1Part1s = DoDay1Part1();
-Console.WriteLine($"Maximum calories an Elf is carry is: {MaximumCalories(day1Part1s)}");
+//List<ElfFood> day1Part1s = DoDay1Part1();
+//Console.WriteLine($"Maximum calories an Elf is carry is: {MaximumCalories(day1Part1s)}");
 
+Console.WriteLine($"The top 3 Elves total calories are {DoDay1Part2()}");
 
-//Read through the Day 1, Part 1 input file
+//Read through the Day 1, Part 1 input file. This returns the list of ElfFood in an unordered list
 List<ElfFood> DoDay1Part1()
 {
     var tmpList = new List<ElfFood>();
@@ -35,6 +36,18 @@ List<ElfFood> DoDay1Part1()
     return tmpList;
 }
 
+// Return the top 3 elves with calories
+int DoDay1Part2()
+{
+    var unOrderedElfFoodList = DoDay1Part1();
+    var orderedList = unOrderedElfFoodList;
+
+    orderedList.Sort();
+    orderedList.Reverse();
+
+    return orderedList[0].TotalCalories() + orderedList[1].TotalCalories() + orderedList[2].TotalCalories();
+}
+
 int MaximumCalories(List<ElfFood> list)
 {
     int max = 0;
@@ -49,7 +62,9 @@ int MaximumCalories(List<ElfFood> list)
 
 static string[] ReadInputFile()
 {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
     string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
     string txtFilePath = path.Replace("\\bin", "\\Assets\\Day1 input.txt");
 
     string[] lines = File.ReadAllLines(txtFilePath);
