e62ae9123f6fe6a60289637c531e2ca098874aee
Rod Falanga <rfalanga@users.noreply.github.com>
2023-03-25 08:33:53 -0600

    added new class and 1 method

---
+++ AdventOfCode2022/AdventOfCode2022/TryingEditorConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    /*
+     * This class isn't important, I'm just using it to learn more how the .editorconfig file
+     * affects new classes added to the project.
+     */
+    internal class TryingEditorConfig
+    {
+        int Add2(int x, int y)
+        { 
+            return x + y; 
+        }
+    }
+}
